d013f207643b0e8aec0c01699a3d8ffcf4cf072b
Amanda Hasenzahl <amandahasenzahl@Amandas-MacBook-Pro.local>
2021-09-23 14:02:05 -0500

    Get cards to flip over on click

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,5 +1,5 @@
 <template>
-    <button class="card-button">
+    <button class="card-button" @click="flipCards(card)">
         <img
             :src="card.front"
             alt="Charlotte vs Sasha Banks at Hell in a Cell 2016"
@@ -26,9 +26,16 @@
 </template>
 
 <script>
+/* eslint-disable no-param-reassign */
 export default {
     name: 'Card',
     props: ['card'],
+    methods: {
+        flipCards(card) {
+            card.isOpen = ! card.isOpen;
+            card.isShown = ! card.isShown;
+        },
+    },
 };
 </script>
 
